7f6d93bd23357851e4cc12ecd5ebaf0ea44476b1
fengde <derek.feng@mkcorp.com>
2019-07-04 16:06:03 +0800

    Initial Commit

---
+++ tssc/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  tssc
+//
+//  Created by fengde on 2019/7/4.
+//  Copyright © 2019 fengde. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
