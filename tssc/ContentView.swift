dcc4f8ac1bbe836bbc82a59c3ac191afa48767f1
fengde <derek.feng@mkcorp.com>
2019-07-11 15:48:37 +0800

    update ui

--- tssc/ContentView.swift
+++ tssc/ContentView.swift
@@ -11,15 +11,44 @@ import SwiftUI
 struct ContentView : View {
     var body: some View {
         NavigationView {
-//            List(tangshiData.identified(by: \.id)) { item in
-//                NavigationLink(destination: TangShiDetail(tangshi: item)) {
-//                    TangShiRow(tangshi: item)
-//                }
-//            }
-            VStack() {
-                HStack {
-                    Text("十大诗人")
-                        .font(.headline)
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    VStack(alignment: .center, spacing: 2) {
+                        Text("今日推荐")
+                            .background(Color.green)
+                        Text("今日推荐")
+                            .background(Color.red)
+                    }
+                    .frame(minWidth:0, maxWidth: .infinity, minHeight: 100, maxHeight: 100)
+                    .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+                    SectionView(title: "十大诗人")
+                    SectionView(title: "十大知名诗")
+                    SectionView(title: "五言绝句")
+                }
+            }
+            .navigationBarTitle(Text("唐诗三百首"))
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
+struct SectionView : View {
+    var title: String
+    
+    var body: some View {
+        return VStack() {
+            HStack {
+                Text(title)
+                    .font(.headline)
                     Spacer()
                 }
                 .padding(.leading)
@@ -32,21 +61,11 @@ struct ContentView : View {
                             .frame(width: 80)
                             .frame(height: 100)
                             .border(Color.gray, width: 1, cornerRadius: 8)
-                        
+                            
                         }
                     }
-                }.padding()
-            }
-            .padding(.top)
+                    }.padding()
+                }
+                .padding(.top)
         }
-// .navigationBarTitle(Text("唐诗三百首"))
     }
-}
-
-#if DEBUG
-struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-#endif
