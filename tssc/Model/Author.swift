f8cd94d7eeab23334140aa50c74794b3afffd955
fengde <derek.feng@mkcorp.com>
2019-07-15 17:24:42 +0800

    merge files

--- tssc/Model/Author.swift
+++ tssc/Model/Author.swift
@@ -2,28 +2,15 @@
 //  Author.swift
 //  tssc
 //
-//  Created by feng qijun on 2019/7/13.
+//  Created by fengde on 2019/7/12.
 //  Copyright © 2019 fengde. All rights reserved.
 //
 
 import Foundation
-import SwiftUI
 
 struct Author: Hashable, Codable {
     var name: String
     var nickname: String
-    var bgColors: [Double]
+    var summary: String
     var baikeUrl: String
-    
-    func bgRed() -> String {
-        return String(bgColors[0])
-    }
-    
-    func bgGreen() -> String {
-        return String(bgColors[1])
-    }
-    
-    func bgBlue() -> String {
-        return String(bgColors[2])
-    }
 }
